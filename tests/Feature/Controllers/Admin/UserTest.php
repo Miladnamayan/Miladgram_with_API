b7bf78bcb26db4ae427de7fb2cce6ebc89a5bfbe
miladnamayan <miladnamayan@50>
2024-02-25 19:11:34 +0330

    set_user-Test

--- tests/Feature/Controllers/Admin/UserTest.php
+++ tests/Feature/Controllers/Admin/UserTest.php
@@ -40,31 +40,39 @@ class UserTest extends TestCase
     public function testShowListOfAllUsersForAdmin(): void
     {
         Sanctum::actingAs($this->admin);
-        $response = $this->getJson(route('admin.list'));
-        $this->assertEquals('MILAD',$response->json()[0]['name']);
-        $this->assertEquals('admin',$response->json()[3]['role']);
-        $this->assertEquals(5,count( $response->json()));
-        $response
-        ->assertStatus(200)
-        ->assertJsonStructure([
+
+        $response = $this->getJson(route('admin.list'))
+            ->assertStatus(200)
+            ->assertJsonStructure([
             '*' =>[
             'id',
             'email',
             'name',
             'role'
         ]]);
+
+        // dd($response->json());
+        $this->assertEquals('MILAD',$response->json()[0]['name']);
+        $this->assertEquals('admin',$response->json()[3]['role']);
+        $this->assertEquals(7,count( $response->json()));
     }
 
     public function testDontShowListOfUsersForNonAdminRoles(): void
     {
         Sanctum::actingAs($this->nonAdminRoles);
-        $response = $this->getJson(route('admin.list'));
-        $response->assertStatus(403);
+
+        $response = $this->getJson(route('admin.list'))
+        ->assertStatus(403)
+        ->json();
+
+        $this->assertEquals('Only Admin is allowed',$response['error']);
+
     }
 
     public function testShowAUserForAdmin(): void
     {
         Sanctum::actingAs($this->admin);
+
         $response = $this->getJson(route('admin.show',$this->user))
             ->assertStatus(200)
             ->assertOk()
@@ -75,38 +83,54 @@ class UserTest extends TestCase
     public function testDontShowAUsersForNonAdminRoles(): void
     {
         Sanctum::actingAs($this->nonAdminRoles);
-        $response = $this->getJson(route('admin.show',$this->user));
-        $response->assertStatus(403);
+
+        $response = $this->getJson(route('admin.show',$this->user))
+        ->assertStatus(403)
+        ->json();
+
+        $this->assertEquals('Only Admin is allowed',$response['error']);
     }
 
     public function testDeleteUserForAdmin(): void
     {
         Sanctum::actingAs($this->admin);
-        $response = $this->deleteJson(route('admin.delete',$this->user->id));
-        $response->assertStatus(200);
-        $this->assertDatabaseMissing($this->user->getTable(), [
-            'id' => $this->user->id,
-        ]);
+
+        $response = $this->deleteJson(route('admin.delete',$this->user->id))
+        ->assertStatus(200)
+        ->json();
+
+        $this->assertEquals('success',$response['Status']);
+        $this->assertEquals('Delete user Successful',$response['message']);
+        $this->assertDatabaseMissing($this->user->getTable(),['id' => $this->user->id,]);
+
+
+
     }
 
     public function testNotAllowingDeletionUserForNonAdminRoless(): void
     {
         Sanctum::actingAs($this->nonAdminRoles);
-        $response = $this->deleteJson(route('admin.delete',$this->user->id));
-        $response->assertStatus(403);
-        $this->assertDatabaseHas($this->user->getTable(), [
-            'id' => $this->user->id,
-        ]);
+
+        $response = $this->deleteJson(route('admin.delete',$this->user->id))
+        ->assertStatus(403)
+        ->json();
+
+        $this->assertEquals('Only Admin is allowed',$response['error']);
+        $this->assertDatabaseHas($this->user->getTable(),['id' => $this->user->id,]);
     }
 
     public function testAcceptUserForAdmin(): void
     {
         Sanctum::actingAs($this->admin);
-        $response = $this->deleteJson(route('admin.delete',$this->user->id));
-        $response->assertStatus(200);
-        $this->assertDatabaseMissing($this->user->getTable(), [
-            'id' => $this->user->id,
-        ]);
+
+        $response = $this->postJson(route('admin.accept',$this->user->id))
+        ->assertStatus(200)
+        ->json();
+
+        $this->assertEquals('0',$this->user->status);
+        $this->assertEquals('1',$response['data']['status']);
+        $this->assertEquals('success',$response['Status']);
+        $this->assertEquals('Accept Author successful',$response['message']);
     }
 
 }
